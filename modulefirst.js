350226e0499181e2ce2c91fb26a0341953cd54a3
farish <developer.websites1@gmail.com>
2021-10-11 23:52:47 +0530

    es6 vs cjs

---
+++ modulefirst.js
@@ -0,0 +1,10 @@
+// const simple = require("./modulesecond");
+
+// import {simple, simple2} from "./modulesecond.mjs"
+// import  simple23 from "./modulesecond.mjs"
+import  * as a2 from "./modulesecond.mjs"
+
+// simple();
+// simple2();
+// simple23();
+console.log(a2.simple());
